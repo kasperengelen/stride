ec0fc891b5929335cb3a8180f38cdb3c5f2141b8
DawidMiroyan <dawid_miroyan@hotmail.com>
2019-03-09 15:45:05 +0100

    Added plots for 2.2 and updated script for 2.2

--- Scripts/2_2_Vaccinating/2_2_vacinating_on_campus.py
+++ Scripts/2_2_Vaccinating/2_2_vacinating_on_campus.py
@@ -6,32 +6,19 @@ import sys
 
 from pystride.Event import Event, EventType
 from pystride.PyController import PyController
-from pystride.stride.pystride import ToId, ToString, NumOfTypes
+from pystride.stride.pystride import Id_College
+
+vaccinatedFolder = "Vaccinated"
+notVaccinatedFolder = "NotVaccinated"
 
 def vaccinate(simulator, event):
     if event.timestep == 7:
-        print("BANZAI")
         pop = simulator.GetPopulation()
-       
-        tot_count = 0
-        tot_student_count = 0        
-
-        appel = ToString(2)
-        print(appel)
-
         for pIndex in range(pop.size()):
-            # No idea if this works
-            person = pop[pIndex]          
-            poolId = 2
-
-            if person.GetAge() >= 18 and person.GetAge() <= 26:
-                tot_count += 1
-            if person.IsInPool(poolId):
-                tot_student_count += 1
-                # This pop is a student
+            if pop[pIndex].GetPoolId(Id_College):
                 if pop[pIndex].GetHealth().IsSusceptible():
                     pop[pIndex].GetHealth().SetImmune()
-        print(tot_count, tot_student_count, tot_student_count/tot_count * 100)
+        
 def trackCases(simulator, event):
     """
         Callback function to track cumulative cases
@@ -47,60 +34,117 @@ def trackCases(simulator, event):
             writer.writeheader()
         writer.writerow({"timestep": timestep, "cases": cases})
 
-def plotNewCases(outputPrefix, vaccinationLevels):
+def plotNewCases(outputPrefix, iterations):
     """
-        Plot new cases per day for a list of vaccination levels.
+        Plot new cases per day.
     """
-    for v in vaccinationLevels:
-        days = []
-        newCasesPerDay = []
-        prevCumulativeCases = 0 # Keep track of how many cases have been recorded until current time-step
-        with open(os.path.join(outputPrefix + "_" + str(v), "cases.csv")) as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                days.append(int(row["timestep"]))
-                cumulativeCases = int(row["cases"])
-                newCasesPerDay.append(cumulativeCases - prevCumulativeCases)
-                prevCumulativeCases = cumulativeCases
-        plt.plot(days, newCasesPerDay)
+    days = range(0, 50)
+    for v in [False, True]:
+        totalNewCases = [0]*50
+        for i in range(0, iterations):
+            cumulativeCasesPerDay = []
+            
+            if v:
+                folderName = os.path.join(outputPrefix, vaccinatedFolder + "_" + str(i))
+            else:
+                folderName = os.path.join(outputPrefix, notVaccinatedFolder + "_" + str(i))
+                
+            with open(os.path.join(folderName, "cases.csv")) as csvfile:
+                reader = csv.DictReader(csvfile, fieldnames=["timestep", "cases"])
+                row1 = next(reader)
+                
+                for row in reader:
+                    cumulativeCases = int(row["cases"])
+                    cumulativeCasesPerDay.append(cumulativeCases)
+                    
+            prevCumulativeCases = cumulativeCasesPerDay[0] # Keep track of how many cases have been recorded until current time-step
+            for j in range(len(cumulativeCasesPerDay)):
+                totalNewCases[j] += cumulativeCasesPerDay[j] - prevCumulativeCases
+                prevCumulativeCases = cumulativeCasesPerDay[j]
+                
+        averageCasesPerDay = [i/iterations for i in totalNewCases]
+        plt.plot(days, averageCasesPerDay)
+        
     plt.xlabel("Simulation day")
     plt.ylabel("New cases per day")
-    plt.legend(["60% immune", "80% immune"])
+    plt.legend(["Students are vaccinated", "Students are not vaccinated"])
     plt.show()
 
-def runSimulation(outputPrefix, vaccinate_callback):
+def plotCumulativeCases(outputPrefix, iterations):
+    """
+        Plot cumulative cases per day.
+    """
+    days = range(0, 50)
+    for v in [False, True]:
+        totalNewCases = [0]*50
+        for i in range(0, iterations):
+            cumulativeCasesPerDay = []
+            
+            if v:
+                folderName = os.path.join(outputPrefix, vaccinatedFolder + "_" + str(i))
+            else:
+                folderName = os.path.join(outputPrefix, notVaccinatedFolder + "_" + str(i))
+                
+            with open(os.path.join(folderName, "cases.csv")) as csvfile:
+                reader = csv.DictReader(csvfile, fieldnames=["timestep", "cases"])
+                row1 = next(reader)
+                
+                for row in reader:
+                    cumulativeCases = int(row["cases"])
+                    cumulativeCasesPerDay.append(cumulativeCases)
+                    
+            for j in range(len(cumulativeCasesPerDay)):
+                totalNewCases[j] += cumulativeCasesPerDay[j]
+                
+        averageCasesPerDay = [i/iterations for i in totalNewCases]
+        plt.plot(days, averageCasesPerDay)
+        
+    plt.xlabel("Simulation day")
+    plt.ylabel("Cumulative cases per day")
+    plt.legend(["Students are vaccinated", "Students are not vaccinated"])
+    plt.show()
+
+def runSimulation(outputPrefix, v, iteration, rng_seed):
     # Set up simulator
     control = PyController(data_dir="data")
     # Load configuration from file
     control.loadRunConfig(os.path.join("config", "run_import_default.xml"))
     # Set some parameters
     
-    
+    if v:
+        outputPrefix = os.path.join(outputPrefix, vaccinatedFolder + "_" + str(iteration))
+    else:
+        outputPrefix = os.path.join(outputPrefix, notVaccinatedFolder + "_" + str(iteration))
+        
     control.runConfig.setParameter("immunity_profile", "AgeDependent")
     control.runConfig.setParameter("immunity_distribution_file", "data/lower_student_immunity.xml")
     control.runConfig.setParameter("immunity_link_probability", 0)   
-
-    # Set population file    
-    control.runConfig.setParameter("population_file", "2_2_gengeopop.proto")
-    control.runConfig.setParameter("population_type", "import")
-    
+    control.runConfig.setParameter("population_file", "data/2_2_gengeopop.proto")
+    control.runConfig.setParameter("geopopulation_file", "data/2_2_gengeopop.proto")
     control.runConfig.setParameter("output_prefix", outputPrefix)
+    control.runConfig.setParameter("rng_seed", rng_seed)
 
     print("config done")
-    if vaccinate_callback:
+    if v:
         control.registerCallback(vaccinate, EventType.Stepped)
-    #control.registerCallback(trackCases, EventType.Stepped)
+    control.registerCallback(trackCases, EventType.Stepped)
     # Run simulation
     control.control()
 
 def main():
     outputPrefix = "2-2"
-    MassVaccinate = [True]
+    iterations = 20
+    MassVaccinate = [[False]*iterations, [True]*iterations]
     # Run simulations
-    for v in MassVaccinate:
-        runSimulation(outputPrefix, v)
+    for i in range(len(MassVaccinate)):                    
+        for j in range(len(MassVaccinate[i])):
+            v = MassVaccinate[i][j]
+            rng_seed = random.randint(0, 10000000)
+            runSimulation(outputPrefix, v, j, rng_seed)
+            
     # Post-processing
-    #plotNewCases(outputPrefix, vaccinationLevels)
+    plotNewCases(outputPrefix, iterations)
+    plotCumulativeCases(outputPrefix, iterations)
 
 if __name__=="__main__":
     main()
