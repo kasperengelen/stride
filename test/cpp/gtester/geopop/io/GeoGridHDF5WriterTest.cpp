dc69267804650bb527f5418ad0631d0fc57eef9b
DawidMiroyan <dawid_miroyan@hotmail.com>
2019-03-26 16:06:30 +0100

    Added HDF5 reader/writer test files

---
+++ test/cpp/gtester/geopop/io/GeoGridHDF5WriterTest.cpp
@@ -0,0 +1,40 @@
+/*
+ *  This is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *  The software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License
+ *  along with the software. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright 2019, ACED.
+ */
+
+#include "GeoGridIOUtils.h"
+
+#include "geopop/ContactCenter.h"
+#include "geopop/GeoGridConfig.h"
+//#include "geopop/io/GeoGridHDF5Writer.h"
+#include "pop/Population.h"
+#include "util/FileSys.h"
+
+#include <gtest/gtest.h>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+using namespace geopop;
+using namespace stride;
+using namespace stride::ContactType;
+using namespace stride::util;
+
+namespace {
+TEST(GeoGridHDF5WriterTest, locationTest) {}
+TEST(GeoGridHDF5WriterTest, contacPoolTest) {}
+TEST(GeoGridHDF5WriterTest, peopleTest) {}
+TEST(GeoGridHDF5WriterTest, commutesTest) {}
+
+} //namespace
