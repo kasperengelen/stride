a8b9004896ef7fbe8c14e493ae7a6b75a4c70429
MeyerJon <Jonathan.Meyer@student.uantwerpen.be>
2019-03-26 10:38:46 +0100

    Trying to use RPATH

--- CMakeCPP.cmake
+++ CMakeCPP.cmake
@@ -182,7 +182,7 @@ set(CMAKE_AUTOMOC ON)
 set(CMAKE_AUTOUIC ON)
 set(CMAKE_AUTORCC ON)
 set(CMAKE_INCLUDE_CURRENT_DIR ON)
-set(CMAKE_PREFIX_PATH "/home/jonathan/Qt/5.12.2/gcc_64/lib/cmake")
+set(CMAKE_PREFIX_PATH "/opt/Qt/5.12.2/gcc_64/lib/cmake")
 find_package(Qt5Widgets REQUIRED)
 find_package(Qt5Location REQUIRED)
 
