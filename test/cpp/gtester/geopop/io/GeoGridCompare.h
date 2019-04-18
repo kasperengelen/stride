1c3d6211468494218f6beb2b3aaca116b1851ffb
IgorSchittekat <igor.schittekat@gmail.com>
2019-04-18 17:11:22 +0200

    Added GeoGridCompare for testing

---
+++ test/cpp/gtester/geopop/io/GeoGridCompare.h
@@ -0,0 +1,32 @@
+#pragma once
+
+
+#include <map>
+#include "pop/Person.h"
+#include "contact/ContactType.h"
+#include "geopop/GeoGrid.h"
+#include "geopop/Location.h"
+
+class GeoGridCompare {
+public:
+        void CompareGeoGrid(const geopop::GeoGrid& geoGrid1, const geopop::GeoGrid& geoGrid2);
+
+private:
+
+        void CompareLocation(geopop::Location& location1, geopop::Location& location2);
+
+        void CompareCoordinate(const geopop::Coordinate& coordinate1, const geopop::Coordinate& coordinate2);
+
+        void CompareContactPools(stride::util::SegmentedVector<stride::ContactPool*>& contactPools1,
+                                 stride::util::SegmentedVector<stride::ContactPool*>& contactPools2);
+
+        void CompareContactPool(stride::ContactPool* contactPool1, stride::ContactPool* contactPool2);
+
+        void ComparePersons();
+
+
+private:
+        std::map<int, stride::Person*> m_persons_found1;
+        std::map<int, stride::Person*> m_persons_found2;
+
+};
