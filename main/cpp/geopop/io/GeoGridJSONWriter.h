5a6153320cd8bd12631d2d1ec1a111d50965d153
IgorSchittekat <igor.schittekat@gmail.com>
2019-04-23 13:18:00 +0200

    init

--- main/cpp/geopop/io/GeoGridJSONWriter.h
+++ main/cpp/geopop/io/GeoGridJSONWriter.h
@@ -18,7 +18,8 @@
 #include "GeoGridWriter.h"
 #include "geopop/Location.h"
 
-#include <boost/property_tree/ptree.hpp>
+//#include <boost/property_tree/ptree.hpp>
+#include "util/json.hpp"
 #include <set>
 
 namespace stride {
@@ -43,20 +44,24 @@ public:
         void Write(GeoGrid& geoGrid, std::ostream& stream) override;
 
 private:
-        /// Create a Boost Property Tree containing all info needed to reconstruct a ContactCenter.
-        boost::property_tree::ptree WriteContactCenter(std::shared_ptr<ContactCenter> contactCenter);
 
-        /// Create a Boost Property Tree containing all info needed to reconstruct a ContactPool.
-        boost::property_tree::ptree WriteContactPool(stride::ContactPool* contactPool);
+        void WriteLocation(nlohmann::json js, const Location& location);
 
-        /// Create a Boost Property Tree containing all info needed to reconstruct a Coordinate.
-        boost::property_tree::ptree WriteCoordinate(const Coordinate& coordinate);
 
-        /// Create a Boost Property Tree containing all info needed to reconstruct a Location.
-        boost::property_tree::ptree WriteLocation(const Location& location);
-
-        /// Create a Boost Property Tree containing all info needed to reconstruct a Person.
-        boost::property_tree::ptree WritePerson(stride::Person* person);
+//        /// Create a Boost Property Tree containing all info needed to reconstruct a ContactCenter.
+//        boost::property_tree::ptree WriteContactCenter(std::shared_ptr<ContactCenter> contactCenter);
+//
+//        /// Create a Boost Property Tree containing all info needed to reconstruct a ContactPool.
+//        boost::property_tree::ptree WriteContactPool(stride::ContactPool* contactPool);
+//
+//        /// Create a Boost Property Tree containing all info needed to reconstruct a Coordinate.
+//        boost::property_tree::ptree WriteCoordinate(const Coordinate& coordinate);
+//
+//        /// Create a Boost Property Tree containing all info needed to reconstruct a Location.
+//        boost::property_tree::ptree WriteLocation(const Location& location);
+//
+//        /// Create a Boost Property Tree containing all info needed to reconstruct a Person.
+//        boost::property_tree::ptree WritePerson(stride::Person* person);
 
 private:
         std::set<stride::Person*> m_persons_found; ///< The persons found when looping over the ContactPools.
