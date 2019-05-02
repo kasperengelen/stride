bb0727594f022124650537e993b4631f5662d42c
kasperengelen <kasper.engelen@student.uantwerpen.be>
2019-05-02 23:49:53 +0200

    added more contact pool types, added temporary view code

--- main/cpp/datavis/model/Locality.cpp
+++ main/cpp/datavis/model/Locality.cpp
@@ -23,31 +23,48 @@
 namespace stride {
 namespace datavis {
 
-Locality::Locality(const std::string& name, geopop::Coordinate coord, std::map<ContactType::Id, PopCategory> popCatMap)
-		: m_coordinate(coord), m_name(name),
-		  m_college_pop(popCatMap.at(ContactType::Id::College)),
-		  m_daycare_pop(popCatMap.at(ContactType::Id::Daycare))
-{}
+const QVariantMap contactPoolView(const ContactPoolStat& pool);
 
-const std::string& Locality::GetName() const
+/**
+ * Construct a QVariantMap that represents a view for this locality.
+ */
+const QVariantMap Locality::GetView() const
 {
-	return m_name;
+	// create map
+	QVariantMap map;
+
+	map.insert("name",   QString::fromStdString(m_name));
+	map.insert("lat",    m_coordinate.get<0>());
+	map.insert("lon",    m_coordinate.get<1>());
+	map.insert("totPop", m_total_pop);
+
+	map.insert("household",     contactPoolView(m_household_pop));
+	map.insert("k12school",     contactPoolView(m_k12school_pop));
+	map.insert("college",       contactPoolView(m_college_pop));
+	map.insert("workplace",     contactPoolView(m_workplace_pop));
+	map.insert("primCommunity", contactPoolView(m_prim_community_pop));
+	map.insert("secCommunity",  contactPoolView(m_sec_community_pop));
+	map.insert("daycare",       contactPoolView(m_daycare_pop));
+	map.insert("preschool",     contactPoolView(m_preschool_pop));
+
+
+	return map;
 }
 
-const geopop::Coordinate& Locality::GetCoordinate() const
+const QVariantMap contactPoolView(const ContactPoolStat& pool)
 {
-	return m_coordinate;
-}
+	QVariantMap map;
 
-const PopCategory& Locality::GetCollegePopData() const
-{
-	return m_college_pop;
-}
+	map.insert("total",          pool.total);
+	map.insert("susceptible",    pool.susceptible);
+	map.insert("exposed",        pool.exposed);
+	map.insert("infectious",     pool.infectious);
+	map.insert("symptomatic",    pool.symptomatic);
+	map.insert("infectAndSympt", pool.infect_and_sympt);
+	map.insert("recovered",      pool.recovered);
+	map.insert("immune",         pool.immune);
 
-
-const PopCategory& Locality::GetDaycarePopData() const
-{
-	return m_daycare_pop;
+	return map;
 }
 
 } // namespace datavis
--- main/cpp/datavis/model/Locality.h
+++ main/cpp/datavis/model/Locality.h
@@ -20,17 +20,50 @@
 
 #pragma once
 
-#include "datavis/model/PopCategory.h"
-
 #include "contact/ContactType.h"
 #include "geopop/Coordinate.h"
 #include "disease/Health.h"
+
+#include <QVariant>
 
 #include <string>
 #include <map>
 
 namespace stride {
 namespace datavis {
+
+/**
+ * Each individual in a locality can belong to one or more contact pools such
+ * as a college, a workplace, etc. This struct keeps track of how many individuals
+ * are part of a certain type of contact pool and number of individuals per
+ * health status.
+ */
+struct ContactPoolStat
+{
+	/// Total number of individuals that belong to this type of contact pool
+	unsigned int total;
+
+	/// The number of individuals with the HealthStatus::Susceptible health status.
+    unsigned int susceptible;
+
+	/// The number of individuals with the HealthStatus::Exposed health status.
+    unsigned int exposed;
+
+	/// The number of individuals with the HealthStatus::Infectious health status.
+	unsigned int infectious;
+
+	/// The number of individuals with the HealthStatus::Symptomatic health status.
+	unsigned int symptomatic;
+
+	/// The number of individuals with the HealthStatus::InfectiousAndSymptomatic health status.
+    unsigned int infect_and_sympt;
+
+	/// The number of individuals with the HealthStatus::Recovered health status.
+    unsigned int recovered;
+
+	/// The number of individuals with the HealthStatus::Immune health status.
+    unsigned int immune;
+};
 
 /**
  * Class that represents a locality. This is a geographical location
@@ -47,34 +80,87 @@ public:
 	 * @param coord The coordinate that represents the geographical location of the locality.
 	 * @param popCatMap An std::map that maps ContactType::Id to PopCategory objects.
 	 */
-	explicit Locality(const std::string& name, geopop::Coordinate coord, std::map<ContactType::Id, PopCategory> popCatMap);
+	explicit Locality(const std::string& name, const geopop::Coordinate& coord, unsigned int totPop,
+					  const ContactPoolStat& householdData, const ContactPoolStat& k12schoolData, const ContactPoolStat& collegeData, const ContactPoolStat& workplaceData,
+					  const ContactPoolStat& primCommunityData, const ContactPoolStat& secCommunityData, const ContactPoolStat& daycareData, const ContactPoolStat& preschoolData)
+		: m_name(name), m_coordinate(coord), m_total_pop(totPop),
+		  m_household_pop(householdData), m_k12school_pop(k12schoolData), m_college_pop(collegeData), m_workplace_pop(workplaceData),
+		  m_prim_community_pop(primCommunityData), m_sec_community_pop(secCommunityData), m_daycare_pop(daycareData), m_preschool_pop(preschoolData)
+	{}
 
 	/**
 	 * Retrieve the place name of the locality.
 	 */
-	const std::string& GetName() const;
+	const std::string& GetName() const { return m_name; }
 
 	/**
 	 * Retrieve the geographical coordinate of the locality.
 	 */
-	const geopop::Coordinate& GetCoordinate() const;
+	const geopop::Coordinate& GetCoordinate() const { return m_coordinate; }
 
+	/**
+	 * Retrieve the total population within the locality.
+	 */
+	unsigned int GetTotalPop() const { return m_total_pop; }
 
-	const PopCategory& GetCollegePopData() const;
+	/**
+	 * Retrieve information about the population that is part of a household.
+	 */
+	const ContactPoolStat& GetHouseholdPopData() const { return m_household_pop; }
 
+	/**
+	 * Retrieve information about the population that is part of a k12 school.
+	 */
+	const ContactPoolStat& GetK12SchoolPopData() const { return m_k12school_pop; }
 
-	const PopCategory& GetDaycarePopData() const;
+	/**
+	 * Retrieve information about the population that is part of a college.
+	 */
+	const ContactPoolStat& GetCollegePopData() const { return m_college_pop; }
 
-	// TODO getters for other pop groups
+	/**
+	 * Retrieve information about the population that is part of a workplace.
+	 */
+	const ContactPoolStat& GetWorkplacePopData() const { return m_workplace_pop; }
+
+	/**
+	 * Retrieve information about the population that is part of a primary community.
+	 */
+	const ContactPoolStat& GetPrimCommunityPopData() const { return m_prim_community_pop; }
+
+	/**
+	 * Retrieve information about the population that is part of a secondary community.
+	 */
+	const ContactPoolStat& GetSecCommunityPopData() const { return m_sec_community_pop; }
+
+	/**
+	 * Retrieve information about the population that is part of a daycare.
+	 */
+	const ContactPoolStat& GetDaycarePopData() const { return m_daycare_pop; }
+
+	/**
+	 * Retrieve information about the population that is part of a preschool.
+	 */
+	const ContactPoolStat& GetPreSchoolPopData() const { return m_preschool_pop; }
+
+	/**
+	 * Construct a QVariantMap that represents a view for this locality.
+	 */
+	const QVariantMap GetView() const;
 
 private:
-	geopop::Coordinate m_coordinate;
 	std::string        m_name;
+	geopop::Coordinate m_coordinate;
+	unsigned int       m_total_pop;
 
-	PopCategory        m_college_pop;
-	PopCategory        m_daycare_pop;
-
-	// TODO add more population groups
+	ContactPoolStat    m_household_pop;
+	ContactPoolStat    m_k12school_pop;
+	ContactPoolStat    m_college_pop;
+	ContactPoolStat    m_workplace_pop;
+	ContactPoolStat    m_prim_community_pop;
+	ContactPoolStat    m_sec_community_pop;
+	ContactPoolStat    m_daycare_pop;
+	ContactPoolStat    m_preschool_pop;
 };
 
 } // namespace datavis
