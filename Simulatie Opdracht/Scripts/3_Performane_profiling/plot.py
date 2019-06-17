af5de8c33aa9329e796dc233b975d5920cf1fe43
DawidMiroyan <dawid_miroyan@hotmail.com>
2019-06-17 12:02:46 +0200

    Updated plots for performance profiling

--- Simulatie Opdracht/Scripts/3_Performane_profiling/plot.py
+++ Simulatie Opdracht/Scripts/3_Performane_profiling/plot.py
@@ -1,103 +1,139 @@
 import os
 import matplotlib.pyplot as plt
 import matplotlib.dates as md
-import datetime
+from datetime import datetime
+import time
 
-def time_to_datetime(times, format):
+
+def time_to_datetime(times, format, n=20):
     dates = list()
+
     for i in times:
-        dates.append(datetime.datetime.strptime(i, format))
-        
+        t = datetime.strptime(i, format)
+        td = t - datetime(1900, 1, 1)
+        sec = td.total_seconds()
+        div_time = sec / n
+        fixed_time = datetime.fromtimestamp(div_time)
+
+        f = fixed_time.strftime(format)
+        g = datetime.strptime(f, format)
+        dates.append(g)
+
     return dates
-    
+
+
 # Day plot
-times = ["04:540",
-        "09:489",
-        "14:142",
-        "18:007",
-        "38:496"]
-dates = time_to_datetime(times, "%S:%f")
+times = ["00:59:322",
+         "01:52:714",
+         "02:34:824",
+         "03:09:711",
+         "06:51:445"]
+dates = time_to_datetime(times, "%M:%S:%f")
 days = [50, 100, 150, 200, 500]
 
 fig = plt.figure()
 
-ax=plt.gca()
-xfmt = md.DateFormatter('%S')
+ax = plt.gca()
+xfmt = md.DateFormatter('%-S')
 ax.yaxis.set_major_formatter(xfmt)
 
 plt.xlabel("Days in simulation")
 plt.ylabel("Time in seconds")
-plt.ylim([datetime.datetime.strptime("0", "%S"), datetime.datetime.strptime("45", "%S")])
+plt.ylim([datetime.strptime("0", "%S"), datetime.strptime("30", "%S")])
 
 plt.plot(days, dates, marker='o')
-fig.savefig(os.path.join("plots", "3_GProf_numdays.eps"))
+fig.savefig(os.path.join("plots", "3_numdays.eps"))
 
 # Population size
-times = ["00:421",
-         "00:816",
-         "01:332",
-         "02:249",
-         "04:926"]
+times = ["04:093",
+         "04:533",
+         "05:615",
+         "08:298",
+         "16:479"]
 dates = time_to_datetime(times, "%S:%f")
 pops = [10000, 50000, 100000, 200000, 500000]
 
 fig = plt.figure()
 
-ax=plt.gca()
-xfmt = md.DateFormatter('%S')
+ax = plt.gca()
+xfmt = md.DateFormatter('%-S')
 ax.yaxis.set_major_formatter(xfmt)
 
 plt.xlabel("Population size")
 plt.ylabel("Time in seconds")
-plt.ylim([datetime.datetime.strptime("0", "%S"), datetime.datetime.strptime("7", "%S")])
+plt.ylim([datetime.strptime("0", "%S"), datetime.strptime("5", "%S")])
 
 plt.plot(pops, dates, marker='o')
-fig.savefig(os.path.join("plots", "3_GProf_popsize.eps"))
+fig.savefig(os.path.join("plots", "3_genpop_popsize.eps"))
+
+# Population size
+times = ["06:923",
+         "10:203",
+         "15:210",
+         "25:015",
+         "52:985"]
+dates = time_to_datetime(times, "%S:%f")
+pops = [10000, 50000, 100000, 200000, 500000]
+
+fig = plt.figure()
+
+ax = plt.gca()
+xfmt = md.DateFormatter('%-S')
+ax.yaxis.set_major_formatter(xfmt)
+
+plt.xlabel("Population size")
+plt.ylabel("Time in seconds")
+plt.ylim([datetime.strptime("0", "%S"), datetime.strptime("5", "%S")])
+
+plt.plot(pops, dates, marker='o')
+fig.savefig(os.path.join("plots", "3_sim_popsize.eps"))
 
 # Immunity rate
-times = ["05:817",
-         "05:750",
-         "05:643",
-         "05:580",
-         "05:648"]
-dates = time_to_datetime(times, "%S:%f")
+times = ["01:04:794",
+         "01:03:841",
+         "01:03:329",
+         "01:01:007",
+         "01:02:708"]
+dates = time_to_datetime(times, "%M:%S:%f")
 imm = [0.2, 0.4, 0.6, 0.8, 1.0]
 
 fig = plt.figure()
 
-ax=plt.gca()
-xfmt = md.DateFormatter('%S')
+ax = plt.gca()
+xfmt = md.DateFormatter('%-S')
 ax.yaxis.set_major_formatter(xfmt)
 
 plt.xlabel("Immunity rate")
 plt.ylabel("Time in seconds")
-plt.ylim([datetime.datetime.strptime("3", "%S"), datetime.datetime.strptime("7", "%S")])
+plt.ylim([datetime.strptime("0", "%S"), datetime.strptime("5", "%S")])
 
 plt.plot(imm, dates, marker='o')
-fig.savefig(os.path.join("plots", "3_GProf_immunityrate.eps"))
+fig.savefig(os.path.join("plots", "3_immunityrate.eps"))
 
 # Seeding rate
-times = ["06:599",
-         "05:589",
-         "05:134",
-         "04:865"]
-dates = time_to_datetime(times, "%S:%f")
-seed = [0.2, 0.02, 0.002, 0.0002]
+times = ["01:16:650",
+         "01:16:384",
+         "01:16:063",
+         "01:10:787",
+         "01:00:997",
+         "00:52:866",]
+dates = time_to_datetime(times, "%M:%S:%f")
+seed = [0.1, 0.06, 0.02, 0.006, 0.002, 0.0002]
 
 fig = plt.figure()
 
-ax=plt.gca()
-xfmt = md.DateFormatter('%S')
+ax = plt.gca()
+xfmt = md.DateFormatter('%-S')
 ax.yaxis.set_major_formatter(xfmt)
 
 plt.xlabel("Seeding rate")
 plt.ylabel("Time in seconds")
-plt.ylim([datetime.datetime.strptime("3", "%S"), datetime.datetime.strptime("7", "%S")])
+plt.ylim([datetime.strptime("0", "%S"), datetime.strptime("7", "%S")])
 
 plt.plot(seed, dates, marker='o')
-fig.savefig(os.path.join("plots", "3_GProf_seedingrate.eps"))
+fig.savefig(os.path.join("plots", "3_seedingrate.eps"))
 
-# Contact log mode 
+# Contact log mode
 """times = ["00:27:06",
          "00:26:48",
          "00:00:06",
@@ -105,17 +141,16 @@ fig.savefig(os.path.join("plots", "3_GProf_seedingrate.eps"))
 
 dates = time_to_datetime(times, "%H:%M:%S")"""
 labels = ["Susceptibles",
-         "All",
-         "Transmission",
-         "None"]
+          "All",
+          "Transmission",
+          "None"]
 fig = plt.figure()
-x = [1,2,3,4]
-ax=plt.subplot()
+x = [1, 2, 3, 4]
+ax = plt.subplot()
 
 plt.xlabel("Contact log modes")
 plt.ylabel("Time in minutes")
 
 ax.bar(x, [27.48, 26.48, 0.6, 0.5], tick_label=labels)
 
-fig.savefig(os.path.join("plots", "3_GProf_contactlogmode.eps"))
-
+fig.savefig(os.path.join("plots", "3_contactlogmode.eps"))
