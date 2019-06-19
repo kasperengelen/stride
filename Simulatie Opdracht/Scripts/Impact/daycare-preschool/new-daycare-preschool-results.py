bbde2182382bad02db0edf0934b1a1a89e143f36
MeyerJon <Jonathan.Meyer@student.uantwerpen.be>
2019-06-19 17:16:59 +0200

    Added Daycare/Preschool impact

--- Simulatie Opdracht/Scripts/Impact/daycare-preschool/new-daycare-preschool-results.py
+++ Simulatie Opdracht/Scripts/Impact/daycare-preschool/new-daycare-preschool-results.py
@@ -36,8 +36,10 @@ def plotNewCases(outputPrefix, iterations, daycare_fractions, preschool_fraction
             totalNewCases = [0]*day
             for i in range(0, iterations):
                 cumulativeCasesPerDay = []
+                dc_frac = daycare_fractions[fraction_ix]
+                ps_frac = preschool_fractions[fraction_ix]
 
-                folderName = os.path.join(outputPrefix, f"DC-{daycare_fractions[fraction_ix]}-PS-{preschool_fractions[fraction_ix]}_{i}")
+                folderName = os.path.join(outputPrefix, f"DC-{dc_frac}-PS-{ps_frac}_{i}")
                     
                 with open(os.path.join(folderName, "cases.csv")) as csvfile:
                     reader = csv.DictReader(csvfile, fieldnames=["timestep", "cases"])
@@ -75,8 +77,10 @@ def plotCumulativeCases(outputPrefix, iterations, daycare_fractions, preschool_f
             totalNewCases = [0]*day
             for i in range(0, iterations):
                 cumulativeCasesPerDay = []
+                dc_frac = daycare_fractions[fraction_ix]
+                ps_frac = preschool_fractions[fraction_ix]
 
-                folderName = os.path.join(outputPrefix, f"DC-{daycare_fractions[fraction_ix]}-PS-{preschool_fractions[fraction_ix]}_{i}")
+                folderName = os.path.join(outputPrefix, f"DC-{dc_frac}-PS-{ps_frac}_{i}")
                     
                 with open(os.path.join(folderName, "cases.csv")) as csvfile:
                     reader = csv.DictReader(csvfile, fieldnames=["timestep", "cases"])
@@ -101,6 +105,7 @@ def plotCumulativeCases(outputPrefix, iterations, daycare_fractions, preschool_f
     fig.savefig(os.path.join(outputPrefix, 'cumulative_cases_{}runs.eps'.format(iterations)))
 
 def runSimulation(outputPrefix, daycare_fraction, preschool_fraction, workplace, iteration, rng_seed, days):
+    print(f"DC: {daycare_fraction} / PS: {preschool_fraction} - Iteration {iteration}")
     # Set up simulator
     control = PyController(data_dir="data")
     # Load configuration from file
@@ -117,18 +122,18 @@ def runSimulation(outputPrefix, daycare_fraction, preschool_fraction, workplace,
     control.runConfig.setParameter("geopop_gen.participation_preschool", preschool_fraction)
     control.runConfig.setParameter("geopop_gen.participation_daycare", daycare_fraction)
 
-    print("Config done")
     control.registerCallback(trackCases, EventType.Stepped)
     # Run simulation
     control.control()
+    print("Simulation done")
 
 def main():
     iterations = 20
     days = 50
     outputPrefix = os.path.join("daycare-preschool", f"{iterations}runs_{days}days_seeding0.002")
     # These must be the same size, they'll line up in the simulated scenarios
-    daycare_fractions   = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
-    preschool_fractions = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
+    daycare_fractions   = [0.2, 0.4, 0.6, 0.8]
+    preschool_fractions = [0.2, 0.4, 0.6, 0.8]
     workplace_files = ["workplace_size_distribution2.csv"]
 
     # Run simulations
@@ -139,8 +144,10 @@ def main():
                 runSimulation(outputPrefix, daycare_fractions[fraction_ix], preschool_fractions[fraction_ix], workplace, iteration, rng_seed, days)
 
     legend = list()
-    for fraction_ix in daycare_fractions:
-            legend.append(f"DC: {daycare_fractions[fraction_ix]} / PS: {preschool_fractions[fraction_ix]}")
+    for fraction_ix in range(len(daycare_fractions)):
+            dc_frac = daycare_fractions[fraction_ix]
+            ps_frac = preschool_fractions[fraction_ix]
+            legend.append(f"DC: {dc_frac} / PS: {ps_frac}")
     # Post-processing
     plotNewCases(outputPrefix, iterations, daycare_fractions, preschool_fractions, workplace_files, days, legend)
     plotCumulativeCases(outputPrefix, iterations, daycare_fractions, preschool_fractions, workplace_files, days, legend)
